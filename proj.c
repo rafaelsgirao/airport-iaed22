b876eafe75d34efcab8f787e2e0c3b18e957c308
Rafael Girão <git@rafael.ovh>
2022-03-23 14:54:17 +0000

    Change everything to english

--- proj.c
+++ proj.c
@@ -3,108 +3,162 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define LIM_NOME_AEROPORTO 30+1
-#define LIM_NOME_CIDADE 50+1
-#define LIM_CODIGO_VOO 6+1
-#define LIM_DATA 10+1
-
-#define MAX_AEROPORTOS 40
-
+#include "proj.h"
 
 /*-------------------*/
-/*----Protótipos-----*/
-/*-------------------*/
-void novoAeroporto();
-void listaAeroportos();
-void adicionaVoo();
-void listaVoosAeroporto();
-void avancaData();
-
-/*-------------------*/
-/*-----Estruturas----*/
-/*-------------------*/
-typedef struct {
-	int dia;
-	int mes;
-	int ano;
-	int hora;
-	int minuto;
-} Data;
-/*-------------------*/
-/*-Variáveis Globais-*/
+/*-Global Variables--*/
 /*-------------------*/
 
-/*long data; TODO*/ 
+Date system_date;
+int airport_count;
+
+Airport airports[MAX_AIRPORTS];
 
 /*-------------------*/
-/*------Funções------*/
+/*-----Functions-----*/
 /*-------------------*/
 int main() {
 	char command;
 	while ((command = getchar()) != 'q') {
 		switch (command) {
 		case 'a':
-			novoAeroporto();
+			addAirport();
 			break;
 		case 'l':
-			listaAeroportos();
+			listAirports();
 			break;
 		case 'v':
-			adicionaVoo();
+			addFlight();
 			break;
 		case 'p':
-			listaVoosAeroporto();
+			listAirportFlights();
 			break;
 		case 't':
-			avancaData();
+			advanceDate();
 		}
 	}
 	return 0;
 }
 
-void avancaData()
-{
-	Data data;
-	int dia, mes, ano
-	scanf("%d%d%d", &dia, &mes, &ano);
-	if (dia <= 0 || mes <= 0 || ano <= 0) {
+
+void addAirport() {
+	Airport arprt;
+	int i = 0;
+
+	/*Read Airport ID and country*/
+	scanf("%s%s", arprt.id, arprt.country);
+	while (arprt.id[i] != '\0') {
+		if (!isupper(arprt.id[i++])) {
+			printf("invalid airport ID\n");
+			return;
+		}
+	}
+	if (airport_count == MAX_AIRPORTS) {
+		printf("too many airports\n");
+		return;
+	}
+	for (i=0; i < airport_count; i++) {
+		if (strcmp(arprt.id, airports[i].id) == 0) {
+			printf("duplicate airport\n");
+			return;
+		}
+	}
+	/*Read Airport city, can be a string with spaces*/
+	fgets(arprt.city, LIM_CITY_NAME ,stdin);
+	/*TODO: store the new airport, ordered alphabetically*/
+	airports[airport_count++] = arprt;
+	printf("airport %s\n", arprt.id);
+	return;
+	}
+
+
+void listAirports() {
+	int i;
+	char c;
+	Airport arprt;
+	/*Case of command without arguments*/
+	if ((c = getchar() == '\n')) {
+		for(i=0;i<airport_count;i++) {
+			arprt = airports[airport_count];
+			/*TODO: implement storing flight count for each airport*/
+			printf("%s %s %s #%d", arprt.id, arprt.city, arprt.country, 0);
+		}
+		return;
+	}
+
+	/*TODO: implement*/
+	return;
+}
+
+void addFlight() {
+	/*TODO: implement*/
+	return;
+}
+void listAirportFlights() {
+	/*TODO: implement*/
+	return;
+}
+
+Date getDate() {
+	/*Getchar in between fgets to consume dash character */
+	Date newdate;
+/*	char rawdate[LIM_DATE];*/
+	char rawday[2+1], rawmonth[2+1], rawyear[4+1];
+
+	fgets(rawday, 2+1, stdin);
+	getchar();
+	fgets(rawmonth, 2+1, stdin);
+	getchar();
+	fgets(rawday, 2+1, stdin);
+	getchar();
+
+	newdate.day = atoi(rawday);
+	newdate.month = atoi(rawmonth);
+	newdate.year = atoi(rawyear);
+	return newdate;
+}
+
+void advanceDate() {
+	Date date = getDate();
+	scanf("%d%d%d", &date.day, &date.month, &date.year);
+	if (date.day <= 0 || date.month <= 0 || date.year <= 0 || compareDate(system_date, date)) {
 		printf("invalid date\n");
 		return;
 	}
 
-	/*data.ano += ano; - irrelevant because max data <= 1 yr*/
-	data.mes += mes;
-	data.dia += dia;
+	/*date.year += ano; - TODO: make sure this is irrelevant because max data <= 1 yr*/
+	system_date.month += date.month;
+	system_date.day += date.day;
 	
 
 	return;
 }
 
 /*
-	Returns -1 if data1 in the past relative to data2
+	Returns 1 if data1 in the past relative to data2 (delta(data) > 0)
 	0 if both are the same date
-	1 if date is in the future
+	-1 if date is in the future
 */
-int comparaData(Data data1, Data data2) {
+int compareDate(Date date1, Date date2) {
 	/*Comparar ano*/
-	if (data1.ano > data2.ano) {
+	if (date2.year - date1.year > 0) {
 		return 1;
 	}
-	else if (data1.ano > data2.ano) {
+	else if (date2.year - date1.year < 0) {
 		return -1;
 	}
 	/*Comparar mês*/
-	if (data1.mes > data2.mes) {
+	if (date2.month - date1.month > 0) {
 		return 1;
 	}
-	else if (data1.mes < data2.mes) {
+	else if (date2.month - date1.month < 0) {
 		return -1;
 	}
 	/*Comparar dia*/
-	if (data1.dia > data2.dia) {
+	if (date1.day> date2.day) {
 		return 1;
 	}
-	else if(data1.dia < data2.dia) {
+	else if(date1.day < date2.day) {
 		return -1;
 	}
 	/*Datas são iguais */
